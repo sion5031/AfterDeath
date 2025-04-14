621e0a52ac429c818ee556e648ebe679629633a8
sion5031 <swseo2787@gmail.com>
2025-04-14 12:43:40 +0900

    main에 merge

--- AfterDeath/Run.cpp
+++ AfterDeath/Run.cpp
@@ -1,35 +1,102 @@
+#include <windows.h>
+
+#include "GameManager.h"
 #include "Creature.h"
+#include "Player.h"
+#include "Monster.h"
 #include "Map.h"
 #include "Item.h"
+#include "ConsoleGotoxy.h"
+
+unique_ptr<Map> CurrentMap;
+
 
 int main()
 {
+	//콘솔 창 크기 변경
+	system("mode con:cols=130 lines=35");
+
+	//커서 숨기기
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.bVisible = 0;
+	cursorInfo.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+
+	srand(time(NULL));
+	//=============위는 건들지 말기
 
 
 
+	GameManager* GM = new GameManager();
+	system("cls");
+
+	if (true)
+	{
+		//오프닝
+		SetConsoleOutputCP(CP_UTF8);
+		GM->PrintOpeningMessage("오프닝 문구");
+		SetConsoleOutputCP(CP_UTF8);
+		GM->PrintOpeningPage("Main_After", 13, 4);
+		Sleep(500);
+		GM->PrintOpeningPage("Main_Death", 12, 18);
+		SetConsoleOutputCP(949);
+		Gotoxy(36, 31);
+		cout << "PRESS ANY KEY";
+		FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+		_getche();
+		GotoxyCll(1);
+		system("cls");
+
+		SetConsoleOutputCP(CP_UTF8);
+		GM->PrintStory("오프닝 스토리");
+		SetConsoleOutputCP(949);
+
+		system("cls");
+	}
+
+	int end = true;
+
+	while (end)
+	{
+		GM->PrintDeathMessage();
 
 
+		while (true)
+		{
+			//Gotoxy(0, 0);
+			GM->GetCurrentMap()->PrintMap();
 
+			int mapOrHead = GM->GetCurrentMap()->MovePlayer();
+			if (mapOrHead == -100)
+			{
+				end = false;
+				break;
+			}
+			else if (mapOrHead != -1)
+			{
+				GM->MoveMap(mapOrHead);
+				continue;
+			}
+			if (GM->DeathPlayerChecker())
+			{
+				break;
+			}
+			GM->DeathMonsterChecker();
 
+			GM->GetCurrentMap()->MoveMonster();
+			if (GM->DeathPlayerChecker())
+			{
+				break;
+			}
+			GM->DeathMonsterChecker();
+		}
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	GM->PrintStory("엔딩 스토리");
 
 
 	return 0;
 }
-
 
 
 
---
+++ AfterDeath/Skill.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Creature;
+
+class Skill
+{
+private:
+
+protected:
+	string Name;
+	int Type; //0Àº Active / 1Àº Passive
+	int Level;
+	int MpConsume;
+	int EffectValue;
+	int Duration;
+	int Turn;
+	bool Using = false;
+	string Explanation;
+
+public:
+	Skill();
+	//virtual ~Skill(){ cout << "~Skill()" << endl; }
+	virtual ~Skill() {}
+
+	virtual string Effect(shared_ptr<Creature> player, int turn) = 0;
+	void PlusLevel(int level);
+
+	void SetName(string name);
+	void SetType(int type);
+	void SetLevel(int level);
+	void SetEffectValue(int value);
+
+	string GetName();
+	string GetEnhancedName();
+	int GetType();
+	int GetLevel();
+	int GetEffectValue();
+	int GetMpConsume();
+	virtual string GetExplanation() = 0;
+
+};
